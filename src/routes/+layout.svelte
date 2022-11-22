62d97b021e1ebdbf44d68d6b5c51b6f0694c67ec
Code in Forest <36024040+codeinforest@users.noreply.github.com>
2022-11-23 01:34:59 +0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { SvelteUIProvider, AppShell, Navbar, Title, Header } from "@svelteuidev/core";
+</script>
+
+<SvelteUIProvider>
+  <script>
+    import {
+      fns,
+      AppShell,
+      Navbar,
+      Header,
+      Title,
+      Divider,
+    } from "@svelteuidev/core";
+  </script>
+
+  <AppShell>
+    <Navbar slot="navbar">
+      <Title>Title</Title>
+    </Navbar>
+    <Header slot="header">
+      <Title>Header</Title>
+    </Header>
+
+    <slot />
+  </AppShell>
+</SvelteUIProvider>
---
+++ src/routes/profile/+layout.svelte
@@ -0,0 +1,15 @@
+
+<script>
+	import { fns, AppShell, Navbar, Header, Title, Divider } from '@svelteuidev/core';
+</script>
+
+<AppShell>
+	<Navbar slot="navbar">
+        <Title order={2}>Profile Title</Title>
+	</Navbar>
+	<Header slot="header">
+        <Title>Profile Header</Title>
+	</Header>
+
+	<slot>This is the main content</slot>
+</AppShell>
